cd49f3d4749f3033b64085c8f6c83728a5e5be03
prajapatiaditya663-cell <prajpatiaditya663@gmail.comgit config --global user.name prajapatiaditya663-cellgit config --global user.email prajpatiaditya663@gmail.comgit config --global user.email prajpatiaditya663@gmail.com>
2026-02-17 14:29:54 +0530

    .

---
+++ app/robot.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://viral-nest-website-structure.vercel.app/sitemap.xml',
+  }
+}
